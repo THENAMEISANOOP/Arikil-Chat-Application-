dc424a7959159aef28acf32c71912d7efeeeb0a7
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-05-07 01:01:57 +0530

    send friend request

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
-
+import User from "../models/User.js";
+import FriendRequest from "../models/FriendRequest.js";
 
 export async function getRecommendedUsers(req,res){
     try {
@@ -43,6 +44,74 @@ export async function getMyFriends(req,res){
         })
     } catch (error) {
         console.error("Error fetching friends:", error);
+        res.status(500).json({
+            success:false,
+            message:"Internal server error",
+            error:error.message
+        })
+        
+    }
+}
+
+export async function sendFriendRequest(req,res){
+    try {
+        const myId=req.user.id;
+        const {id:recipientId}=req.params;
+
+        // prevent self friend request
+        if(myId===recipientId){
+            return res.status(400).json({
+                success:false,
+                message:"You cannot send friend request to yourself"
+            })
+        }
+        const recipient=await User.findById(recipientId);
+        if(!recipient){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        // already friends
+        if(recipient.friends.includes(myId)){
+            return res.status(400).json({
+                success:false,
+                message:"You are already friends with this user"
+            })
+        }
+        // already sent friend request
+        const existingRequest=await FriendRequest.findOne({
+            $or:[
+                { sender:myId, recipient:recipientId },
+                { sender:recipientId, recipient:myId }
+            ]
+        });
+        if(existingRequest){
+            return res.status(400).json({
+                success:false,
+                message:"Friend request already sent"
+            })
+        }
+        // create friend request
+        const friendRequest=await FriendRequest.create({
+            sender:myId,
+            recipient:recipientId
+        });
+        // **additional step: add friend request to recipient's notifications
+        // send notification to recipient
+        // recipient.notifications.push({
+        //     type:"friend-request",
+        //     message:`${req.user.fullName} sent you a friend request`,
+        //     sender:myId,
+        //     isRead:false
+        // });
+        res.status(200).json({
+            success:true,
+            message:"Friend request sent successfully",
+            data:friendRequest
+        })
+    } catch (error) {
+        console.error("Error sending friend request:", error);
         res.status(500).json({
             success:false,
             message:"Internal server error",
