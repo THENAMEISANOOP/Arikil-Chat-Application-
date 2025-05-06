a3f1efb008d3f573ae05ba8afc65675a6bad759e
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-05-07 01:02:21 +0530

    send frnd request

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getMyFriends, getRecommendedUsers } from '../controllers/user.controller.js';
+import { getMyFriends, getRecommendedUsers, sendFriendRequest } from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.use(protectRoute); // Protect all routes in this file
@@ -8,5 +8,7 @@ router.use(protectRoute); // Protect all routes in this file
 router.get('/',getRecommendedUsers);
 
 router.get('/friends',getMyFriends);
+
+router.post('/friend-request/:id',sendFriendRequest);
 
 export default router;
