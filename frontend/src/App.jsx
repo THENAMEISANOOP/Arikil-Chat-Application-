b80b21431087f5d0795e70cef3ebaebb5b75f73b
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-05-08 18:33:27 +0530

    signup page Complte

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route ,Navigate} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
@@ -14,26 +14,28 @@ import  {axiosInstance}  from './lib/axios';
 
 const App = () => {
   // tanstack query
-  const {data,isLoading,error}=useQuery({
-    queryKey: "todos",
+  const {data:authData,isLoading,error}=useQuery({
+    queryKey: ['authUser'],
     queryFn: async () => {
       const res= await axiosInstance.get('/auth/me')
       return res.data
     },
     retry: false,
   })
-  console.log(data)
+  const authUser=authData?.user
+  
+
   
   return (
-    <div className="h-screen" data-theme="coffee">
+    <div className='h-screen' data-theme="dark" >
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifications" element={<NotificationPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>} />
+        <Route path="/signup" element={!authUser ? <SignUpPage />: <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser  ?  <LoginPage />: <Navigate to="/"/>} />
+        <Route path="/notifications" element={authUser ? <NotificationPage /> : <Navigate to="/login"/>} />
+        <Route path="/call" element={authUser ? <CallPage /> : <Navigate to="/login"/>} />
+        <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/login"/>} />
+        <Route path="/onboarding" element={authUser ? <OnboardingPage /> : <Navigate to="/login"/>} />
       </Routes>
       <Toaster/>
     </div>
